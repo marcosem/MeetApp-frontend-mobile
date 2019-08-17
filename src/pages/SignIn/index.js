5f530bc1041d83506861e90660661a76a937156c
Marcos Mathias <marcos-github@memathias.com>
2019-08-17 16:05:33 -0300

    Styled Input and Button components

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-// import { Container } from './styles';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 import Background from '~/components/Background';
 
 export default function SignIn() {
   return (
     <Background>
       <Text>SignIn</Text>
+      <Input icon="call" placeholder="Enter your name" />
+
+      <Button>Enter</Button>
     </Background>
   );
 }
