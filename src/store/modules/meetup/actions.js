f0b2feeff39911334bb8068af98f54dd9b65e7ce
Marcos Mathias <marcos-github@memathias.com>
2019-08-19 14:21:15 -0300

    Rebuilt Meetup Reducers, Actions, and Sagas

--- src/store/modules/meetup/actions.js
+++ src/store/modules/meetup/actions.js
@@ -1,70 +1,18 @@
-export function selectMeetupRequest(data) {
+export function subscribeMeetupRequest(id, title) {
   return {
-    type: '@meetup/SELECT_MEETUP_REQUEST',
-    payload: data,
-  };
-}
-
-export function unselectMeetupRequest() {
-  return {
-    type: '@meetup/UNSELECT_MEETUP_REQUEST',
-  };
-}
-
-export function updateMeetupRequest(data) {
-  return {
-    type: '@meetup/UPDATE_MEETUP_REQUEST',
-    payload: data,
-  };
-}
-
-export function updateMeetupSuccess(data) {
-  return {
-    type: '@meetup/UPDATE_MEETUP_SUCCESS',
-    payload: data,
-  };
-}
-
-export function updateMeetupFailure() {
-  return {
-    type: '@meetup/UPDATE_MEETUP_FAILURE',
-  };
-}
-
-export function addMeetupRequest(data) {
-  return {
-    type: '@meetup/ADD_MEETUP_REQUEST',
-    payload: data,
-  };
-}
-
-export function addMeetupSuccess() {
-  return {
-    type: '@meetup/ADD_MEETUP_SUCCESS',
-  };
-}
-
-export function addMeetupFailure() {
-  return {
-    type: '@meetup/ADD_MEETUP_FAILURE',
-  };
-}
-
-export function cancelMeetupRequest(id, title) {
-  return {
-    type: '@meetup/CANCEL_MEETUP_REQUEST',
+    type: '@meetup/SUBSCRIBE_MEETUP_REQUEST',
     payload: { id, title },
   };
 }
 
-export function cancelMeetupSuccess() {
+export function subscribeMeetupSuccess() {
   return {
-    type: '@meetup/CANCEL_MEETUP_SUCCESS',
+    type: '@meetup/SUBSCRIBE_MEETUP_SUCCESS',
   };
 }
 
-export function cancelMeetupFailure() {
+export function subscribeMeetupFailed() {
   return {
-    type: '@meetup/CANCEL_MEETUP_FAILURE',
+    type: '@meetup/SUBSCRIBE_MEETUP_FAILED',
   };
 }
